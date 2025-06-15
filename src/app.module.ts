db7bb2ea446f800a7d3989972c9a4e1aab4f97a5
mouloud240 <m.hasrane@esi-sba.dz>
2025-06-15 02:18:52 +0100

    feat:worker:setup worker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { ConfigModule } from '@nestjs/config';
+
+import { BullModule } from '@nestjs/bullmq';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import appConfig from './config/app.config';
+import { AppConfig } from './config/interfaces/app-config.interface';
+import { QUEUE_NAME } from './common/constants/queues';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -11,6 +15,29 @@ import appConfig from './config/app.config';
       validationSchema: null, // You can define a Joi schema here for validation if needed
       load: [appConfig],
     }),
+    BullModule.forRootAsync({
+      useFactory: async (configService:ConfigService) => {
+        const redisHost = configService.get<AppConfig['redis']['host']>('redis.host','localhost');
+        const redisPort = configService.get<AppConfig['redis']['port']>('redis.port', 6379);
+        const redisUrl = `redis://${redisHost}:${redisPort}`;
+        return {
+          connection: {
+            host: redisHost,
+            port: redisPort,
+            url: redisUrl,
+          },
+           
+        };
+      },
+  
+      inject :[ConfigService] 
+      
+    
+    }),
+    BullModule.registerQueue(...Object.values(QUEUE_NAME).map((queueName) => ({
+        name: queueName,
+      })),
+    )
   ],
   controllers: [AppController],
   providers: [AppService],
