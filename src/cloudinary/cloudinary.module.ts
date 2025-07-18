b638f578c53f01945e7e064fdde22c1c56d6e4fd
Mouloud <mouloud@DESKTOP-J2LG27Q.localdomain>
2025-07-18 06:00:27 +0100

    feat:added cloudinary uploads

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CloudinaryModule } from 'nestjs-cloudinary';
+import { config } from 'process';
+import { CloudinaryWrapperServiec } from './cloudinary.service';
 
-@Module({})
-export class CloudinaryModule {}
+@Module({
+  imports: [
+    CloudinaryModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        api_key: configService.get('cloudinary.apiKey'),
+      }),
+    }),
+  ],
+  providers:[CloudinaryWrapperService]
+})
+export class CloudinaryModuleWrapper {}
--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { CloudinaryService } from 'nestjs-cloudinary';
 
 @Injectable()
-export class CloudinaryService {}
+export class CloudinaryWrapperService {
+  constructor(private readonly cloudService: CloudinaryService) {}
+  async uploadImage(file: Express.Multer.File) {
+    return this.cloudService.uploadFile(file);
+  }
+}
