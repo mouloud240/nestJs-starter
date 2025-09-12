7c0dcbd1cf034db35a908258f38f27006beb0343
mouloud <m.hasrane@esi-sba.dz>
2025-09-12 17:36:26 +0100

    refactor(filter):added logs to filter

--- src/global/filter/httpException.filter.ts
+++ src/global/filter/httpException.filter.ts
@@ -3,18 +3,22 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  Logger,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  logger = new Logger(HttpExceptionFilter.name);
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
-    const status = exception.getStatus()
+    const status = exception.getStatus();
     const message = exception.message || 'Internal server error';
 
+    this.logger.warn(`HTTP Exception: Status ${status} - Message: ${message}`);
+    this.logger.error(exception.cause);
     response.status(status).json({
       statusCode: status,
       message: message,
--- src/global/interceptors/logger.interceptor.ts
+++ src/global/interceptors/logger.interceptor.ts
@@ -1,8 +1,14 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Request } from 'express';
 import { Observable, tap } from 'rxjs';
 
 export class LoggerInterceptor implements NestInterceptor {
+  logger = new Logger(LoggerInterceptor.name);
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
@@ -14,14 +20,14 @@ export class LoggerInterceptor implements NestInterceptor {
     const requestPath = request.path;
     const requestMethod = request.method;
 
-    console.log(
+    this.logger.log(
       `🚀 Request started from IP: ${userIp}, Path: ${requestPath}, Method: ${requestMethod} at ${new Date(startTime).toISOString()}`,
     );
     return next.handle().pipe(
       tap(() => {
         const endTime = Date.now();
         const responseTime = endTime - startTime;
-        console.log(
+        this.logger.log(
           `✅ Response completed in ${responseTime}ms ${responseTime < 100 ? '🔥' : responseTime < 300 ? `🌀` : '🐢'}`,
         );
       }),
