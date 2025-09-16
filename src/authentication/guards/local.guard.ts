cff6fdf3efc96e792d088d06f86852791c19954b
mouloud <m.hasrane@esi-sba.dz>
2025-09-16 23:43:27 +0100

    feat(gql):added gql context for localAuthGuard

--- src/authentication/guards/local.guard.ts
+++ src/authentication/guards/local.guard.ts
@@ -1,7 +1,14 @@
+import { ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 export class LocalGuard extends AuthGuard('local') {
   constructor() {
     super();
+
+  }
+  getRequest(context: ExecutionContext) {
+    const ctx=GqlExecutionContext.create(context);
+    return ctx.getContext().req;
   }
 }
