36099ec5f9749a0bf2f18aac3f5569340c957379
mouloud <m.hasrane@esi-sba.dz>
2025-09-23 13:15:09 +0100

    refactor(search):removed unneded imports

--- src/infrastructure/search/search.module.ts
+++ src/infrastructure/search/search.module.ts
@@ -1,5 +1,4 @@
 import { Global, Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
 
 import { SearchService } from './search.service';
